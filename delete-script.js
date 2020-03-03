d3937988b2b787acd3f47e0f1bbec3a09ba2fd41
Bryce Kehoe <bryce@kehoe.co.nz>
2020-03-03 18:54:34 +1300

    Re-wrote pretty much everything!

--- delete-script.js
+++ delete-script.js
@@ -1,31 +1,72 @@
 const fsp = require('fs').promises
-const recursive = require('recursive-readdir')
-const sizeOf = require('image-size')
+const path = require('path')
+
+const { promisify } = require('util')
+const recursive = promisify(require('recursive-readdir'))
+const sizeOf = promisify(require('image-size'))
+const { bold, red } = require('colors')
 
 module.exports = deleteLowResImages
 
 function deleteLowResImages (directory, resolutionLimit) {
-  recursive(directory, (err, files) => {
-    if (err) {
-      console.error(`Couldn't read directory: ${err.message}`)
-      return
-    }
+  console.log(bold('📜  Initiating script...\n'))
 
-    files.forEach((file) => {
-      const resolutionInMP = calculateResolution(file)
-
-      if (resolutionInMP <= resolutionLimit) {
-        fsp.unlink(file)
-          .then(() => console.log(`File removed: ${file}`))
-          .catch(err => console.error(`Couldn't delete file: ${err.message}`))
-      }
-    })
-  })
+  return getListOfFiles(directory)
+    .then(files => filterLowResImages(files, resolutionLimit))
+    .then(files => deleteFiles(files))
+    .catch(() => console.error(red('Something went wrong 😞')))
+    .finally(() => console.log(bold(`\n👋  That's all folks!`)))
 }
 
-function calculateResolution (file) {
-  const dimensions = sizeOf(file)
-  const resolutionInMP = (dimensions.width / 1000) * (dimensions.height / 1000)
+function getListOfFiles (directory) {
+  console.log(bold('📂  Getting list of files...\n'))
 
-  return resolutionInMP
+  return recursive(directory)
+    .catch(err => {
+      console.error(`Couldn't read directory: ${directory}`)
+      throw err
+    })
+}
+
+function filterLowResImages (files, resolutionLimit) {
+  console.log(bold('🧮  Calculating image resolutions...'))
+
+  const promiseChain = files.map(file => calculateResolution(file))
+  return Promise.all(promiseChain)
+    .then(resolutions => {
+      console.log(`Resolutions:`, resolutions)
+      return files.filter((file, index) => resolutions[index] <= resolutionLimit)
+    })
+    .catch(err => {
+      console.error(red(`Calculation failed: ${err.message}`))
+      throw err
+    })
+
+  function calculateResolution (file) {
+    return sizeOf(file)
+      .then(dimensions => (dimensions.width / 1000) * (dimensions.height / 1000))
+  }
+}
+
+function deleteFiles (files) {
+  if (files.length !== 0) {
+    console.log(bold('\n🔨  Deleting files...'))
+
+    return files.forEach(file =>
+      fsp.unlink(file)
+        .then(() => console.log(`Deleted: ${getFilename(file)}`))
+        .catch(err => {
+          console.error(red(`Couldn't delete file: ${file}`))
+          throw err
+        }))
+  } else {
+    console.log(bold('\n👍  No files to delete!'))
+  }
+
+  function getFilename (filePath) {
+    const pathSplit = filePath.split(path.sep)
+    const filename = pathSplit[pathSplit.length - 1]
+
+    return filename
+  }
 }
